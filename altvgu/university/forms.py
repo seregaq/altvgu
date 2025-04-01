7ec957dab942a24dfa52cbad94971bdb1316d97e
seregaq <manuilov_99@bk.ru>
2025-04-01 18:47:14 +0700

    ready lab8

--- altvgu/university/forms.py
+++ altvgu/university/forms.py
@@ -1,6 +1,13 @@
 from django import forms
+from .models import News
 
 class FeedbackForm(forms.Form):
     name = forms.CharField(label="Ваше имя", max_length=100)
     email = forms.EmailField(label="Ваш email", required=False)
     message = forms.CharField(label="Отзыв", widget=forms.Textarea)
+
+
+class NewsForm(forms.ModelForm):
+    class Meta:
+        model = News
+        fields = ['title', 'content', 'category', 'tags']
--- altvgu/university/models.py
+++ altvgu/university/models.py
@@ -6,7 +6,23 @@ class PublishedModel(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(actual = News.Status.actual)
 
+class Category(models.Model):
+    name = models.CharField(max_length=100,
+    db_index=True)
+    slug = models.SlugField(max_length=255,
+    unique=True, db_index=True)
 
+    def __str__(self):
+        return self.name
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
 
 
 class News(models.Model):
@@ -14,7 +30,10 @@ class News(models.Model):
         DRAFT = 0, 'Старая'
         actual = 1, 'Актуальная'
 
+    tags = models.ManyToManyField(Tag, related_name='news')
+    author = models.OneToOneField(Author, on_delete=models.CASCADE, null=True, blank=True)
     objects = models.Manager()
+    category =  models.ForeignKey('Category', on_delete=models.CASCADE)
     published = PublishedModel()
     title = models.CharField(max_length=255, verbose_name="Заголовок")
     content = models.TextField(verbose_name="Содержание")
